b56ac388a3fc09877e2eca33340e3941669880bb
Malay Dewangan <malaydewangan310@gmail.com>
2025-12-23 13:29:08 +0530

    feat(): introduce PagerDuty plugin

---
+++ src/main/java/io/kestra/plugin/pagerduty/PagerDutyExecution.java
@@ -0,0 +1,73 @@
+package io.kestra.plugin.pagerduty;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.tasks.VoidOutput;
+import io.kestra.core.plugins.notifications.ExecutionInterface;
+import io.kestra.core.plugins.notifications.ExecutionService;
+import io.kestra.core.runners.RunContext;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Map;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Send a PagerDuty message with the execution information.",
+    description = "The message will include a link to the execution page in the UI along with the execution ID, namespace, flow name, the start date, duration, and the final status of the execution. If failed, then the task that led to the failure is specified.\n\n" +
+        "Use this notification task only in a flow that has a [Flow trigger](https://kestra.io/docs/administrator-guide/monitoring#alerting). Don't use this notification task in `errors` tasks. Instead, for `errors` tasks, use the [PagerDutyAlert](https://kestra.io/plugins/plugin-pagerduty/io.kestra.plugin.pagerduty.pagerdutyalert) task."
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Send a PagerDuty notification on a failed flow execution.",
+            full = true,
+            code = """
+                id: failure_alert
+                namespace: company.team
+
+                tasks:
+                  - id: send_alert
+                    type: io.kestra.plugin.pagerduty.PagerDutyExecution
+                    url: "{{ secret('PAGERDUTY_EVENT') }}" # format: https://events.pagerduty.com/v2/enqueue
+                    payloadSummary: "PagerDuty Alert"
+                    deduplicationKey: "dedupkey"
+                    routingKey: "routingkey"
+                    eventAction: "acknowledge"
+                    executionId: "{{trigger.executionId}}"
+
+                triggers:
+                  - id: failed_prod_workflows
+                    type: io.kestra.plugin.core.trigger.Flow
+                    conditions:
+                      - type: io.kestra.plugin.core.condition.ExecutionStatus
+                        in:
+                          - FAILED
+                          - WARNING
+                      - type: io.kestra.plugin.core.condition.ExecutionNamespace
+                        namespace: prod
+                        prefix: true
+                """
+        )
+    }
+)
+public class PagerDutyExecution extends PagerDutyTemplate implements ExecutionInterface {
+    @Builder.Default
+    private final Property<String> executionId = Property.ofExpression("{{ execution.id }}");
+    private Property<Map<String, Object>> customFields;
+    private Property<String> customMessage;
+
+    @Override
+    public VoidOutput run(RunContext runContext) throws Exception {
+        this.templateUri = Property.ofValue("pagerduty-template.peb");
+        this.templateRenderMap = Property.ofValue(ExecutionService.executionMap(runContext, this));
+
+        return super.run(runContext);
+    }
+}
