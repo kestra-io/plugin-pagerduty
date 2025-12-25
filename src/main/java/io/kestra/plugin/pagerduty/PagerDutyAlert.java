a6744f1229fa27d1491ef096b6c9a0a212e77284
Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>
2025-12-25 12:55:14 +0530

    feat(): introduce PagerDuty plugin (#4)

---
+++ src/main/java/io/kestra/plugin/pagerduty/PagerDutyAlert.java
@@ -0,0 +1,126 @@
+package io.kestra.plugin.pagerduty;
+
+import io.kestra.core.http.HttpRequest;
+import io.kestra.core.http.HttpResponse;
+import io.kestra.core.http.client.HttpClient;
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.tasks.VoidOutput;
+import io.kestra.core.runners.RunContext;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.net.URI;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Send a PagerDuty alert.",
+    description = "Add this task to a list of `errors` tasks to implement custom flow-level failure notifications. Check the <a href=\"https://developer.pagerduty.com/docs/ZG9jOjExMDI5NTgx-send-an-alert-event\">PagerDuty documentation</a> for more details.."
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Send a PagerDuty alert on a failed flow execution.",
+            full = true,
+            code = """
+                id: unreliable_flow
+                namespace: company.team
+
+                tasks:
+                  - id: fail
+                    type: io.kestra.plugin.scripts.shell.Commands
+                    runner: PROCESS
+                    commands:
+                      - exit 1
+
+                errors:
+                  - id: alert_on_failure
+                    type: io.kestra.plugin.pagerduty.PagerDutyAlert
+                    url: "{{ secret('PAGERDUTY_EVENT') }}" # https://events.pagerduty.com/v2/enqueue
+                    payload: |
+                      {
+                        "dedup_key": "samplekey",
+                        "routing_key": "samplekey",
+                        "event_action": "trigger",
+                        "payload" : {
+                            "summary": "PagerDuty alert",
+                        }
+                      }
+                """
+        ),
+        @Example(
+            title = "Send a Discord message via incoming webhook.",
+            full = true,
+            code = """
+                id: discord_incoming_webhook
+                namespace: company.team
+
+                tasks:
+                  - id: send_pagerduty_alert
+                    type: io.kestra.plugin.pagerduty.PagerDutyAlert
+                    url: "{{ secret('PAGERDUTY_EVENT') }}"
+                    payload: |
+                      {
+                        "dedup_key": "samplekey",
+                        "routing_key": "samplekey",
+                        "event_action": "acknowledge"
+                      }
+                """
+        ),
+    },
+    aliases = "io.kestra.plugin.notifications.pagerduty.PagerDutyAlert"
+)
+public class PagerDutyAlert extends AbstractPagerDutyConnection {
+
+    @Schema(
+        title = "PagerDuty event URL"
+    )
+    @PluginProperty(dynamic = true)
+    @NotBlank
+    protected String url;
+
+    @Schema(
+        title = "PagerDuty message payload"
+    )
+    protected Property<String> payload;
+
+    @Override
+    public VoidOutput run(RunContext runContext) throws Exception {
+        String url = runContext.render(this.url);
+
+        try (HttpClient client = new HttpClient(runContext, super.httpClientConfigurationWithOptions())) {
+            String payload = runContext.render(this.payload).as(String.class).orElse(null);
+
+            runContext.logger().debug("Send Discord webhook: {}", payload);
+            HttpRequest.HttpRequestBuilder requestBuilder = createRequestBuilder(runContext)
+                .addHeader("Content-Type", "application/json")
+                .uri(URI.create(url))
+                .method("POST")
+                .body(HttpRequest.StringRequestBody.builder()
+                    .content(payload)
+                    .build());
+
+            HttpRequest request = requestBuilder.build();
+
+            HttpResponse<String> response = client.request(request, String.class);
+
+            runContext.logger().debug("Response: {}", response.getBody());
+
+            if (response.getStatus().getCode() == 200) {
+                runContext.logger().info("Request succeeded");
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/io/kestra/plugin/pagerduty/PagerDutyTemplate.java
@@ -0,0 +1,110 @@
+package io.kestra.plugin.pagerduty;
+
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.tasks.VoidOutput;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.serializers.JacksonMapper;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.apache.commons.io.IOUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+public abstract class PagerDutyTemplate extends PagerDutyAlert {
+
+    @Schema(
+        title = "Template to use",
+        hidden = true
+    )
+    protected Property<String> templateUri;
+
+    @Schema(
+        title = "Map of variables to use for the message template"
+    )
+    protected Property<Map<String, Object>> templateRenderMap;
+
+    @Schema(
+        title = "Integration Key for an integration on a PagerDuty service"
+    )
+    protected Property<String> routingKey;
+
+    @Schema(
+        title = "Deduplication key for correlating triggers and resolves"
+    )
+    @PluginProperty(dynamic = true)
+    protected Property<String> deduplicationKey;
+
+    @Schema(
+        title = "The type of event. Can be trigger, acknowledge or resolve."
+    )
+    protected Property<String> eventAction;
+
+    @Schema(
+        title = "Brief text summary of the event, used to generate the summaries/titles of any associated alerts."
+    )
+    @Size(max = 1024)
+    @PluginProperty(dynamic = true)
+    protected String payloadSummary;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public VoidOutput run(RunContext runContext) throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> payload = new HashMap<>();
+
+        final var renderedTemplateUri = runContext.render(this.templateUri).as(String.class);
+        if (renderedTemplateUri.isPresent()) {
+            String template = IOUtils.toString(
+                Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream(renderedTemplateUri.get())),
+                StandardCharsets.UTF_8
+            );
+
+            String render = runContext.render(template, templateRenderMap != null ?
+                runContext.render(templateRenderMap).asMap(String.class, Object.class) :
+                Map.of()
+            );
+            map = (Map<String, Object>) JacksonMapper.ofJson().readValue(render, Object.class);
+            payload = (Map<String, Object>) map.getOrDefault("payload", new HashMap<>());
+        }
+
+        if (runContext.render(routingKey).as(String.class).isPresent()) {
+            map.put("routing_key", runContext.render(routingKey).as(String.class).get());
+        }
+
+        if (runContext.render(deduplicationKey).as(String.class).isPresent()) {
+            map.put("dedup_key", runContext.render(deduplicationKey).as(String.class).get());
+        }
+
+        if (runContext.render(eventAction).as(String.class).isPresent()) {
+            map.put("event_action", runContext.render(eventAction).as(String.class).get());
+        }
+
+        if (this.payloadSummary != null) {
+            payload.put("summary", runContext.render(payloadSummary));
+        }
+
+        payload.put("timestamp", LocalDateTime.now());
+
+        map.replace("payload", payload);
+
+        this.payload = Property.ofValue(JacksonMapper.ofJson().writeValueAsString(map));
+
+        return super.run(runContext);
+    }
+
+}
